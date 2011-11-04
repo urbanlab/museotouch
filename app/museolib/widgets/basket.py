5d2a5a33ea67bd57ec8aaebe3777db956162ea2f
triselectif <triselectif@gmx.com>
2011-11-05 00:00:56 +0100

    threaded http requests added cancel and delete functions to the basket

--- app/museolib/widgets/basket.py
+++ app/museolib/widgets/basket.py
@@ -8,11 +8,6 @@ from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
-from kivy.animation import Animation
-
-import urllib2#, urllib
- 
-
 #from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
@@ -22,12 +17,14 @@ from kivy.logger import Logger
 from kivy.graphics import Color, Line, Rectangle#, LineWidth
 #from kivy.clock import Clock
 from kivy.core.image import Image
+from kivy.animation import Animation
+from kivy.network.urlrequest import UrlRequest
 
 from os.path import join, split, abspath, splitext#, dirname, exists
 from os import listdir 
 from json import loads
 from random import random
-
+import urllib2#, urllib
 
 class EmailForm(Widget):
     app = ObjectProperty(None)
@@ -60,7 +57,8 @@ class Basket(Button):
     counter = NumericProperty(0)
     enlarged = BooleanProperty(False)
     email = StringProperty( '' )
-    id_basket = StringProperty( '' )  
+    id_basket = StringProperty( '' ) 
+    url_code = StringProperty( '' ) 
 
     def __init__(self, **kwargs):
         super(Basket,self).__init__(**kwargs)
@@ -119,54 +117,51 @@ class Basket(Button):
         return True  
                
     def validate_basket(self,a):
-        objects = self.objects
-        if not (self.active and len(objects)>0) : 
+        if not (self.active and len(self.objects)>0) : 
             return
-
         if self.enlarged : return
-
-        #in a specific thread in order to not block the app while waiting for an http answer?
-        id_basket,url_code = self.api_create_basket()
-        self.api_add_objects(objects, id_basket)
-        self.api_url_send(id_basket,url_code)
-        self.api_email_send(id_basket)
-        #reset basket
-        if not self.email_send :
-            self.reset()
+        #each process is in a specific thread in order to not block the app while waiting for an http answer
+        #start with that first process
+        self.api_create_basket()
 
     def api_create_basket(self):
         #send to api
         url = self.url+self.api_url
         #create a new basket into the backend, get its id in return
         urla = url + self.api_url_commands['new_basket']
-        answera = self.get_url(urla)
-        try :
-            answera = loads(answera)#decode json
-        except TypeError :
-            print '[ERROR  ] Basket : Backend URL is unreachable: check either url configuration or connectivity' 
-            return
-        id_basket = answera['id']
-        print id_basket
-        url_code = answera['code_url']#url param to retrieve the basket online
+        answera = UrlRequest(urla, self.api_create_basket2, self.api_create_basket_error)
         
-        return (id_basket,url_code)
+    def api_create_basket2(self, req, answera):
+        self.id_basket = id_basket = answera['id']
+        self.url_code = answera['code_url']#url param to retrieve the basket online
+
+        #next step
+        self.api_add_objects(self.objects, id_basket)
+
+    def api_create_basket_error(self, req):
+        print '[ERROR  ] Basket : Backend URL is unreachable: check either url configuration or connectivity' 
 
     def api_add_objects(self, objects, id_basket):
-            #add objects to the basket into the backend
-            idb =''
-            for i in objects :
+        #add objects to the basket into the backend
+        idb =''
+        for i in objects :
                 #add object to the basket
                 if idb == '' : idb = idb + str(i)
                 else : idb = idb + ',' + str(i)
-            suffix = self.api_url_commands['add_object']
-            url = self.url+self.api_url 
-            urlb = url + suffix[0] + idb + suffix[1] + id_basket
-            #send to api
-            answerb = self.get_url(urlb)
-            #answerb = wget(urlb)
-            if not answerb == "OK" : 
-                #print log
-                pass
+        suffix = self.api_url_commands['add_object']
+        url = self.url+self.api_url 
+        urlb = url + suffix[0] + idb + suffix[1] + id_basket
+        #send to api
+        answerb = UrlRequest(urlb, self.api_add_objects2, self.api_add_objects_error)
+        
+    def api_add_objects2(self, req, answerb):
+        id_basket = self.id_basket
+        #next stepS in parallel
+        self.api_url_send(id_basket,self.url_code)
+        self.api_email_send(id_basket)
+
+    def api_add_objects_error(self, req):
+        pass
 
     def api_email_send(self,id_basket):
         #ask the backend to send the basket by email
@@ -182,7 +177,13 @@ class Basket(Button):
         self.val_button = val_button = Button( text = 'ok', size_hint =(None,None),size = (40,40), pos_hint = {'top':1,'right':1} )
         self.val_button.bind(on_press = self.validate_keyboard_input)
         self.parent.add_widget(val_button)
-        self.id_basket = id_basket
+        self.cancel_button = cancel_button = Button( text = 'annul', font_size = 10, size_hint =(None,None),size = (40,30), pos_hint = {'top':0.935,'right':1} )
+        self.cancel_button.bind(on_press = self.cancel_keyboard_input)
+        self.parent.add_widget(cancel_button)
+        self.delete_button = delete_button = Button( text = 'suppr', font_size = 10, size_hint =(None,None),size = (40,30), pos_hint = {'top':0.885,'right':1} )
+        self.delete_button.bind(on_press = self.delete_basket)
+        self.parent.add_widget(delete_button)
+        #self.id_basket = id_basket
 
     def validate_keyboard_input(self,a) :
         email = self.ti.text
@@ -190,42 +191,65 @@ class Basket(Button):
             self.ti.text = 'Email errone'
             return
         self.email = email
-        parent = self.parent
-        parent.remove_widget(self.ti) 
-        parent.remove_widget(self.val_button)
-        self.get_parent_window().release_keyboard()
         #reduce size
-        anim = Animation(size = (self.width / 2, self.height / 2), duration = .25, t='out_quad' )
-        anim.start(self)
-        self.enlarged = False
+        self.reduce_size()
         #continue email_send
         self.api_email_send3()
 
-    def api_email_send3(self):
-                email_add = self.email 
-                id_basket = self.id_basket                
+    def reduce_size(self):
+        parent = self.parent
+        parent.remove_widget(self.ti) 
+        parent.remove_widget(self.val_button)
+        parent.remove_widget(self.cancel_button)
+        parent.remove_widget(self.delete_button)
+        self.get_parent_window().release_keyboard()
+        anim = Animation(size = (self.width / 2, self.height / 2), duration = .25, t='out_quad' )
+        anim.start(self)
+        self.enlarged = False
 
-                suffix = self.api_url_commands['send_basket']
-                url = self.url+self.api_url
-                urlc = url + suffix[0] + id_basket + suffix[1] + email_add 
-                #send to api
-                answerc = self.get_url(urlc)
-                print 'basket : email sent to '+ email_add
-                self.reset()
-                print 'basket : reset'
+    def cancel_keyboard_input(self,a) :
+        self.reduce_size()
+
+    def delete_basket(self,a) :
+        self.reduce_size()
+        self.reset()
+
+    def api_delete_basket(self):
+        return
+        id_basket = self.id_basket
+        suffix = self.api_url_commands['delete_basket']
+        url = self.url+self.api_url
+        urlc2 = url + suffix[0] + id_basket + suffix[1] + email_add
+        answerc = UrlRequest(urlc2)
+        print 'basket : basket number '+ id_basket+' deleted from backend'
+
+    def api_email_send3(self):
+        email_add = self.email 
+        id_basket = self.id_basket                
+
+        suffix = self.api_url_commands['send_basket']
+        url = self.url+self.api_url
+        urlc = url + suffix[0] + id_basket + suffix[1] + email_add 
+        #send to api
+        answerc = UrlRequest(urlc)
+        print 'basket : email sent to '+ email_add
+        self.reset()
+        print 'basket : reset'
 
     def api_url_send(self,id_basket,url_code):
         #ask the backend to send the basket to the specidied url
         if self.url_send :
-                suffix = self.api_url_commands['retrieve_basket_online']
-                urld = self.url + self.retrieve_basket_url + suffix[0] + id_basket + suffix[1] + url_code
-                urle = self.url_send_url + urld
-                try :
-                    answere = self.get_url(urle)
-                    print 'basket : url '+ urle
-                except :
-                    print '[ERROR  ] Basket : Url defined as url_send_url in __init__.py file is unreachable: check either url or connectivity' 
-                    return
+            suffix = self.api_url_commands['retrieve_basket_online']
+            urld = self.url + self.retrieve_basket_url + suffix[0] + id_basket + suffix[1] + url_code
+            urle = self.url_send_url + urld
+            try :
+                answere = self.get_url(urle)
+                print 'basket : url '+ urle
+            except :
+                print '[ERROR  ] Basket : Url defined as url_send_url in __init__.py file is unreachable: check either url or connectivity' 
+                return
+            if not self.email_send :
+                self.reset()
 
    
 
