e6b1a47e627cb9791415f21ab61b816e00848b80
triselectif <triselectif@gmx.com>
2011-11-04 12:28:35 +0100

    extend expo1 with a basket

--- app/expos/1/data/__init__.py
+++ app/expos/1/data/__init__.py
@@ -101,5 +101,27 @@ def build(app):
         ordering_keywords.y = instance.top + 135
     scatter_keywords.bind(pos=set_ordering_keywords_pos)
 
+    #-------------------------------------------------------------------------
+    # Create a basket widget
+    # This button is on the bottom/left part of the screen
+    kwargs = {'pos_hint':{'right': 1, 'top': 1},'size_hint': (None, None), 'size': (64, 64),
+            'border': (0, 0, 0, 0), 'color' : (0,0,0,1), 'bold' : True}
+    #active to False disables the basket
+    #email_send to True activates sending the url of the basket by email
+    #url_send to True activates sending the url of the basket to a specific url 
+    app.basket = basket = Basket(
+        active = True,
+        background_normal='widgets/corner_topright.png',
+        background_down='widgets/corner_topright_down.png',
+        email_send = True,
+        url_send = False,
+        url_send_url = 'http://urltest.lapin.be?url=',
+        app = app, 
+        **kwargs)
+    if basket.active :
+        #do not offer a basket on tablets, only on tables
+        if platform() not in ('android'):  
+            root.add_widget(basket)
+
     return root
 
