5a3773533c4a1f08771952ac2dcf70efbbf1dd17
Antoine <antoine@biin.fr>
2015-10-29 16:02:48 +0100

    Fix : bug sur le bouton exit du menu en haut à droite. A confirmer sur table tactile

--- app/museolib/widgets/menu.py
+++ app/museolib/widgets/menu.py
@@ -18,6 +18,7 @@ class Menu(FloatLayout):
 		self.menu_items={'help':[[Window.width*0.064,0],'help.png','help_down.png',self.help],\
 						'credits':[[Window.width*0.048,Window.width*0.048],'credits.png','credits_down.png',self.credits],\
 						'exit':[[0,Window.width*0.064],'quit.png','quit_down.png',self.exit]}
+	
 	def show_menu(self,instance):
 		# ensures that the menu is always on top
 		parent = self.parent
@@ -27,7 +28,7 @@ class Menu(FloatLayout):
 		#---------------------------------------
 		for item in self.menu_items.values():
 			b = Button(background_normal=item[1],size_hint=(None,None),\
-						background_down=item[2],size=(Window.width*0.032,Window.width*0.032),pos=self.button.pos,on_press=item[3])
+						background_down=item[2],size=(Window.width*0.032,Window.width*0.032),pos=self.button.pos,on_release=item[3])
 			b.opacity=0
 			self.add_widget(b)
 			Animation(pos=[self.button.x-item[0][0],self.button.y-item[0][1]],opacity=1,d=0.5,t='out_bounce').start(b)
